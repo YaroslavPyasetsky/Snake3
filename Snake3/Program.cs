1a49b83b8c4afc5d4f3213b8f9f3568b914fc4db
YaroslavPyasetsky <yaroslav.pyasetsky@gmail.com>
2018-02-08 19:04:38 +0200

    less4

--- Snake3/Program.cs
+++ Snake3/Program.cs
@@ -11,6 +11,14 @@ namespace Snake3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+
             Console.ReadLine();
         }
     }
